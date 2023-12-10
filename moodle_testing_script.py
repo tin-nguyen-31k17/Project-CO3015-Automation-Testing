ef332b611ecbcab5c222bd871ea3815270f70bb7
Tin Nguyen <tin.nguyen.31k17@hcmut.edu.vn>
2023-12-10 20:50:46 +0700

    Addin export report function & exception handling

--- moodle_testing_script.py
+++ moodle_testing_script.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 # Initialize the WebDriver
 driver = webdriver.Chrome()
@@ -16,8 +16,19 @@ def click_when_clickable(by, value, timeout=10):
             EC.element_to_be_clickable((by, value))
         )
         element.click()
+        return True
     except NoSuchElementException:
         print(f"Element not found: {value}")
+        return False
+    except TimeoutException:
+        print(f"Timeout while waiting for element: {value}")
+        return False
+
+# Function to write a report to a CSV file
+def write_report(test_name, result, filename='test_report.csv'):
+    with open(filename, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([test_name, result, time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())])
 
 # Level 0: Automation without using data-driven testing approach
 def test_login():
@@ -27,7 +38,7 @@ def test_login():
     logInAccount=driver.find_element(By.ID,"password")
     logInAccount.send_keys("sandbox")
     logInAccount.submit()
-    
+
 def test_course_registration():
     # try to register for a course
     click_when_clickable(By.LINK_TEXT, "My second course")
@@ -40,10 +51,9 @@ def test_forum_posting():
     click_when_clickable(By.ID, "yui_3_18_1_1_1702097450106_51")
     postForum=driver.find_element(By.ID,"id_subject")
     postForum.send_keys("Forum test")
-     postForum=driver.find_element(By.ID,"tinymce")
+    postForum=driver.find_element(By.ID,"tinymce")
     postForum.send_keys("test forum")
-       click_when_clickable(By.ID, "id_submitbutton")
-    
+    click_when_clickable(By.ID, "id_submitbutton")
 
 def test_assignment_submission():
     # Add steps for assignment submission
@@ -53,17 +63,16 @@ def test_assignment_submission():
     driver.implicitly_wait(5)
     click_when_clickable(By.CLASS_NAME, "fp-btn-add")
     click_when_clickable(By.CLASS_NAME, "px-3")
-    
+
     upload_file = os.path.abspath(
         os.path.join(os.path.dirname(__file__), ".", "test.pdf"))
     file_upload = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.NAME, "repo_upload_file")))
-    
+
     file_upload.send_keys(upload_file)
     click_when_clickable(By.CLASS_NAME, "fp-upload-btn")
-   
-    click_when_clickable(By.ID, "id_submitbutton")
 
+    click_when_clickable(By.ID, "id_submitbutton")
 
 # Level 1: Automation using data-driven testing approach
 def run_data_driven_tests(test_data_file):
@@ -94,10 +103,15 @@ def run_tests_with_inputs(test_data_file):
 test_data_file = 'test_cases.csv'
 
 # Run Level 0 tests
-test_login()
-# test_course_registration()
-test_forum_posting()
-test_assignment_submission()
+try:
+    test_login()
+    test_forum_posting()
+    test_assignment_submission()
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    # Write the report
+    write_report('Test Name', 'Pass or Fail')
 
 # Run Level 1 tests
 # run_data_driven_tests(test_data_file)
@@ -105,5 +119,5 @@ test_assignment_submission()
 # Run Level 2 tests
 # run_tests_with_inputs(test_data_file)
 
-# Close the WebDriver
-# driver.quit()
+# Write the report
+write_report('Test Name', 'Pass or Fail')
